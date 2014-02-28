4dc3cf1edf7a9ff71578311a04aa8adb15c78887
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2014-02-28 15:32:48 +0100

    Removed glib dependency

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -6,12 +6,5 @@ mmd_objs = %w[parse_utilities.o parser.o GLibFacade.o writer.o text.o html.o lat
 
 $objs = mmd_objs.map { |s| "../MultiMarkdown-4/#{s}" } + ["multi_markdown.o"]
 
-if pkg_config = find_executable('pkg-config')
-  $CFLAGS = "-fcommon "+`#{pkg_config} --cflags glib-2.0`
-  $LDFLAGS = `#{pkg_config} --libs glib-2.0`
-else
-  fail "glib2 not found"
-end
-
 create_header
 create_makefile('multi_markdown')
